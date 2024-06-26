4bc99f56618f278a774ab3736b7d5eade41bf3f5
adoneabiilesh <hitimu9@gmail.com>
2024-06-26 14:50:23 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <div className='w-full fixed bg-red-700 h-35'>
         <div className='relative justify-between flex flex-row px-4'>
-        <a href="/">Home</a>  
+        <Link href="/">Home</Link>
       
-            <a href="/aboutus"> Chi Siamo</a>
-            <a href="/services">Servizi</a>
-           
-            <a href="/contact">Contatti </a>  
-            
+            <Link href="/aboutus"> Chi Siamo</Link>
+            <Link href="/services">Servizi</Link>
+            <Link href="/contact">Contatti </Link>
         </div>      
     </div>
   )
